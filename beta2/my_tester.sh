d51e279ae69cefad9194d03ac730a281df0d75e1
Liron <lironcohen3@mail.tau.ac.il>
2023-04-21 23:55:08 +0300

    Update my_tester.sh

--- beta2/my_tester.sh
+++ beta2/my_tester.sh
@@ -26,6 +26,18 @@ execute_test "ARG -x -n main.c" 9
 execute_test "{ -x main.c" 10
 execute_test "{ -x -c main.c" 11
 execute_test "{ -x -n main.c" 12
+execute_test "arg -n -b main.c" 13
+execute_test "er.h main.c" 14
+execute_test "ARG -i -v -n main.c" 15
+execute_test "ARG -i -v -c main.c" 16
+execute_test "ARG -i -v -x -c main.c" 17
+execute_test "ARG -i -v -x -n main.c" 18
+execute_test "ARG -x -v main.c" 19
+execute_test "ARG -x -c -v main.c" 20
+execute_test "ARG -x -n -v main.c" 21
+execute_test "{ -x -v main.c" 22
+execute_test "{ -x -c -v main.c" 23
+execute_test "{ -x -n -v main.c" 24
 
 # regex tests
 # ./regex_test1.sh
